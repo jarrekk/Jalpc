ec7d15fa3ad31c58f9285961a8388ef2c002097f
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-09-27 21:28:51 +1300

    improved search code
    Former-commit-id: a36ad9bb3542d0fcd2c44767e9730fdd05e2c3ac

--- search/js/cb-search.js
+++ search/js/cb-search.js
@@ -1,85 +1,64 @@
  $(document).ready(function () {
-        var time1 = 0;
-        var show = false;
-        var names = new Array(); //文章名字等
-        var urls = new Array(); //文章地址
-        $(document).keyup(function (e) {
-            var time2 = new Date().getTime();
-            if (e.keyCode == 17) {
-                var gap = time2 - time1;
-                time1 = time2;
-                if (gap < 500) {
-                    if (show) {
-                        $(".search-tool").css("display", "none");
-                        show = false;
-                    } else {
-                        $(".search-tool").css("display", "block");
-                        show = true;
-                        $("#search-content").val("");
-                        $("#search-content").focus();
-                    }
-                    time1 = 0;
-                }
-			}else if(e.keyCode == 27){
-                    $(".search-tool").css("display", "none");
-                    show = false;
-                    time1 = 0;
-                }
-        });
+    var initial = 0;
+    var show = true;
 
- 		$("#search-content").keyup(function (e) {
-            var time2 = new Date().getTime();
-            if (e.keyCode == 17) {
-                var gap = time2 - time1;
-                time1 = time2;
-                if (gap < 500) {
-                    if (show) {
-                        $(".search-tool").css("display", "none");
-                        show = false;
-                    } else {
-                        $(".search-tool").css("display", "block");
-                        show = true;
-                        $("#search-content").val("");
-                        $("#search-content").focus();
-                    }
-                    time1 = 0;
-                }
-            }
-        });
+    var toggleSearch = function (visible) {
+      initial = 0;
+      show = !visible;
+      var visibility = visible ? 'block' : 'none';
 
-        $("#close-btn").click(function () {
-            $(".search-tool").css("display", "none");
-            show = false;
-            time1 = 0;
-        });
+      if(visible) {
+        $("#search-content").val("");
+        $("#search-content").focus();
+      }
 
-        $("#search-btn").click(function(){
-            $(".search-tool").css("display", "block");
-            show = true;
-            $("#search-content").val("");
-            $("#search-content").focus();
-            time1 = 0;
-        });
+      $(".search-tool").css("display", visibility);
+    };
 
-        $.getJSON("/search/cb-search.json").done(function (data) {
-            if (data.code == 0) {
-                for (var index in data.data) {
-                    var item = data.data[index];
-                    names.push(item.title);
-                    urls.push(item.url);
-                }
-
-                $("#search-content").typeahead({
-                    source: names,
-
-                    afterSelect: function (item) {
-						$(".search-tool").css("display", "none");
-                        show = false;
-                        window.location.href = (urls[names.indexOf(item)]);
-                        return item;
-                    }
-                });
-            }
-        });
-
+    $(document).keyup(function (e) {
+        var now = new Date().getTime();
+        if (e.keyCode == 17) {
+          var gap = now - initial;
+          initial = now;
+          if (gap < 500) {
+            toggleSearch(show);
+          }
+       } else if (e.keyCode == 27) {
+         toggleSearch(false);
+      }
     });
+
+		$("#search-content").keyup(function (e) {
+      var now = new Date().getTime();
+      if (e.keyCode == 17) {
+          var gap = now - initial;
+          initial = now;
+          if (gap < 500) {
+            toggleSearch(!show);
+          }
+      }
+    });
+
+  $("#close-btn").click(function () {
+    toggleSearch(false);
+  });
+
+  $("#search-btn").click(function() {
+    toggleSearch(true);
+  });
+
+  $.getJSON("/search/cb-search.json")
+    .done(function (data) {
+      if (data.code == 0) {
+        $("#search-content").typeahead({
+          source: data.data,
+          displayText: function (item) {
+            return item.title;
+          },
+          afterSelect: function (item) {
+            window.location.href = item.url;
+          }
+        });
+      }
+    });
+});
