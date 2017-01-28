a4348f0e46518632aac171785a48ef6cce13a49b
Jack <me@jack003.com>
2017-01-28 15:32:25 +0800

    fix github_api bug
    Former-commit-id: bea7fd80e1587f345452565a4c3bfc90af675d6e
    Former-commit-id: 146d84443c65078eb5dcf2cbf7b73ce1b7a017f7

--- static/js/github_api.js
+++ static/js/github_api.js
@@ -22,8 +22,8 @@ $(document).ready(function() {
       for  (var i = 0; i < data.length; i++) {
         if (repos.indexOf(data[i].full_name) !== -1) {
           x = data[i].name;
-          $("div[repo=" + x + "]").children(".star").html('<i class="fa fa-star"></i> ' + data[i].stargazers_count)
-          $("div[repo=" + x + "]").children(".fork").html('<i class="fa fa-code-fork"></i> ' + data[i].forks_count)
+          $("div[repo='" + x + "']").children(".star").html('<i class="fa fa-star"></i> ' + data[i].stargazers_count)
+          $("div[repo='" + x + "']").children(".fork").html('<i class="fa fa-code-fork"></i> ' + data[i].forks_count)
         }
       }
     }
