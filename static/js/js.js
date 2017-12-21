6206d8369f3afaf3a3c11b5661e9915583e8025e
jarrekk <me@jarrekk.com>
2017-12-21 17:06:04 +0800

    fix bug #97
    need run build

--- static/js/js.js
+++ static/js/js.js
@@ -4,10 +4,10 @@ $(function(){
     $("div#content").find("p").last().after(buildCodeBlocks);
 
     /* toggle/collapse preformated code blocks */
-    $("pre").bind("click", function(){
-        this.className = this.className.indexOf("collapse") === -1 ?
-            ( "collapse " + this.className ) : this.className.replace("collapse ", "");
-    });
+    //$("pre").bind("click", function(){
+    //    this.className = this.className.indexOf("collapse") === -1 ?
+    //        ( "collapse " + this.className ) : this.className.replace("collapse ", "");
+    //});
     
     /* apply code highlight */
     $('pre code').each( function(i, e) {
